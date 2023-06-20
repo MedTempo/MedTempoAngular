8874c412747c1cc4aa9ec1de287b95988015d3ca
Mateus Felipe Vieira <habdig7@gmail.com>
2023-06-20 15:15:59 -0300

    fixed

---
+++ src/app/tab2/tab2.module.ts
@@ -0,0 +1,26 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tab2Page } from './tab2.page';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
+import { Tab2PageRoutingModule } from './tab2-routing.module';
+
+
+import { NavBarComponentModule } from '../components/nav-bar/nav-bar.module';
+
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    ExploreContainerComponentModule,
+    Tab2PageRoutingModule,
+    NavBarComponentModule
+  ],
+  declarations: [Tab2Page, ]
+})
+export class Tab2PageModule {}
+  
